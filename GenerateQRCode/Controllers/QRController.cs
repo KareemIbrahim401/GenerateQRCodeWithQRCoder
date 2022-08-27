3dcccb9937345e61d25af90dc2861ca9f6372c2e
karee <karee@MSI>
2022-08-27 15:18:29 +0200

    Add project files.

---
+++ GenerateQRCode/Controllers/QRController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace GenerateQRCode.Controllers
+{
+    public class QRController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Booking()
+        {
+            string information = "01099417291";
+
+
+            QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+            QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(information, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qRCodeData);
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (Bitmap bitmap = qrCode.GetGraphic(20))
+                {
+                    bitmap.Save(ms, ImageFormat.Png);
+                    ViewBag.QRCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                    ViewBag.BookingId = information;
+                }
+            }
+            return View();
+        }
+    }
+
+}
